3e897350be56a745f8fe06ca6cc63c4161b05599
mook <mehek@live.com>
2020-08-03 22:08:49 -0700

    back to wav test

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class MeasureBoxWidget extends StatefulWidget {
 }
 
 
-final AudioCache player = new AudioCache(prefix: 'sounds/');
+final AudioCache player = new AudioCache(prefix: 'sounds_wav/');
 
 void _vibrate() async {
   if (await Vibration.hasVibrator() && await Vibration.hasCustomVibrationsSupport()) {
